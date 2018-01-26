dceeab7748ea5615aaae0633e7e6d1d4ee60b2f9
DKrivets <dmitry.krivets@gmail.com>
2018-01-26 21:32:35 +0300

    Add comments and keys.

--- dev-mode.el
+++ dev-mode.el
@@ -1,5 +1,13 @@
-;;; dev-mode.el --- Options for window -*- lexical-binding: t -*-
+;;; dev-mode.el --- Options for window -*- coding: utf-8; lexical-binding: t -*-
+
+;; Author: DKrivets
+;; Version: 0.01
+;; Keywords: dev-mode, languages
+
 ;;; Commentary:
+
+;; Simple mode to editing/programming something
+
 ;;; Code:
 
 (defgroup dev-mode nil "Default group for dev-mode." :group 'application)
@@ -28,19 +36,34 @@
   :type 'integer
   :group 'dev-mode)
 
+(defvar dev-mode-map nil "Keymap for dev-mode.")
+
+(progn
+  ;; Initialize
+  (setq dev-mode-map (make-sparse-keymap))
+
+  ;; Re-start/Re-open new project
+  (define-key dev-mode-map (kbd "C-c C-n") 'dev-mode))
+
 (eval-when-compile (require 'subr-x))
 
 (defun shell-dir (dir)
-  "DIR."
-  (let ((default-directory dir)
-  	(shell-wnd (selected-window))
-  	(shell-buff nil))
-    (setq shell-buf (shell))
-    (if (not (eq (selected-window) shell-wnd))
-  	(progn
-  	  (previous-buffer)
-  	  (other-window -1)
- 	  (switch-to-buffer shell-buf)))))
+  "Run shell with specific DIR or change DIR if shell exists."
+  (if (eql nil (get-buffer "*shell*"))
+      (progn
+	;; If not exist
+	(let ((default-directory dir)
+	      (shell-wnd (selected-window))
+	      (shell-buff nil))
+	  (setq shell-buf (shell))
+	  (if (not (eq (selected-window) shell-wnd))
+	      (progn
+		(previous-buffer)
+		(other-window -1)
+		(switch-to-buffer shell-buf)))))
+    ;; If exist shell
+    (comint-send-string (get-buffer-process (get-buffer "*shell*"))
+			(format "%s %s\n" "cd" dir))))
 
 (defun dev-mode()
   "DEV-MODE main code"
@@ -69,5 +92,7 @@
 
       ;;; Balance area
       (balance-windows-area) )))
+
+(use-local-map dev-mode-map)
 
 (provide 'dev-mode)
